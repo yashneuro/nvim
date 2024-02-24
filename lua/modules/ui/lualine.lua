c629f208ba43da138f77462af7576da71d2eee14
yashneuro <yash@neuropixel.ai>
2024-02-25 04:21:55 +0530

    add transparent lualine backgrounds

--- lua/modules/ui/lualine.lua
+++ lua/modules/ui/lualine.lua
@@ -40,8 +40,8 @@ local config = {
 			-- We are going to use lualine_c an lualine_x as left and
 			-- right section. Both are highlighted by c theme .  So we
 			-- are just setting default looks o statusline
-			normal = { c = { fg = colors.fg, bg = colors.bg } },
-			inactive = { c = { fg = colors.fg, bg = colors.bg } },
+			normal = { c = { fg = colors.fg, bg = "None" } },
+			inactive = { c = { fg = colors.fg, bg = "None" } },
 		},
 	},
 	sections = {
