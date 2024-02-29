16a0fdb14c164129cf3802021d3fe6d260b01416
yashneuro <yash@neuropixel.ai>
2024-03-01 02:36:58 +0530

    add horizontal term

--- lua/modules/tools/config.lua
+++ lua/modules/tools/config.lua
@@ -31,7 +31,7 @@ function config.floaterm()
 		start_in_insert = true,
 		insert_mappings = true,
 		persist_size = true,
-		direction = 'float',
+		direction = 'horizontal',
 		close_on_exit= true,
 		shell = vim.o.shell,
 		float_opts = {
